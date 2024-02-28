4ec96f3f5e02e4fe2e149978d25f0e17d44e7e47
Virangkana <98442623+Virangkana@users.noreply.github.com>
2024-02-28 11:51:19 +0700

    Update valid.js

--- valid.js
+++ valid.js
@@ -15,7 +15,7 @@ function validate(){
 
     if(password.value.length >= 8){
         alert("Login Succesfull");
-        window.location.replace("newpage.html");
+        window.location.replace("map.html");
         return false;
     }
     else{
